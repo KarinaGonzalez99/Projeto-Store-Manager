2cbc2c52e117fb94f714b773aec54576dab4580d
Karina Gonzalez <karinagonzalez99@gmail.com>
2023-05-08 16:46:28 -0300

    Requisito 4

--- src/router/product.router.js
+++ src/router/product.router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const controller = require('../controllers/product.controller');
+const middleware = require('../middlewares/products.validation');
 
 const router = express.Router();
 
@@ -7,6 +8,6 @@ router.get('/', controller.getAll);// geral
 
 router.get('/:id', controller.findById); // baseado no id
 
-router.post('/', controller.insert);
+router.post('/', middleware, controller.insert);
 
 module.exports = router;
